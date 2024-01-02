99aad888c4d3fd4564eba605ec4a079e2027ae99
Hina Nakahira <soleil.colza@icloud.com>
2024-01-02 19:31:14 +0900

    cleaned the project

--- app/src/main/java/com/example/unsplashclient/MainActivity.kt
+++ app/src/main/java/com/example/unsplashclient/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -22,6 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+
                 }
             }
         }
