2b4c03f3054c232a2e1db907df74f1913980e8b0
Hina Nakahira <soleil.colza@icloud.com>
2024-01-17 17:02:32 +0900

    Created the PhotoDetailDto

---
+++ app/src/main/java/com/example/unsplashclient/data/remote/Location.kt
@@ -0,0 +1,13 @@
+package com.example.unsplashclient.data.remote
+
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class Location(
+    val city: String?,
+    val country: String?,
+    val name: String?,
+    val position: Position?
+)
---
+++ app/src/main/java/com/example/unsplashclient/data/remote/PhotoDetailDto.kt
@@ -0,0 +1,31 @@
+package com.example.unsplashclient.data.remote
+
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class PhotoDetailDto(
+    @Json(name = "blur_hash")
+    val blurHash: String?,
+    val color: String?,
+    @Json(name = "created_at")
+    val createdAt: String?,
+    @Json(name = "current_user_collections")
+    val currentUserCollections: List<CurrentUserCollection>?,
+    val description: String?,
+    val downloads: Int?,
+    val exif: Exif?,
+    val height: Int?,
+    val id: String?,
+    @Json(name = "liked_by_user")
+    val likedByUser: Boolean?,
+    val likes: Int?,
+    val links: LinksXX?,
+    val location: Location?,
+    @Json(name = "updated_at")
+    val updatedAt: String?,
+    val urls: UrlsX?,
+    val user: UserX?,
+    val width: Int?
+)
