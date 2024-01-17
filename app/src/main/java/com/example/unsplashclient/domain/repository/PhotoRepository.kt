2aba449cb9a7407fad911a5e17f338813a297e21
Hina Nakahira <soleil.colza@icloud.com>
2024-01-17 13:44:46 +0900

    Created PhotoRepository and set it up

---
+++ app/src/main/java/com/example/unsplashclient/domain/repository/PhotoRepository.kt
@@ -0,0 +1,7 @@
+package com.example.unsplashclient.domain.repository
+
+import com.example.unsplashclient.data.remote.SearchPhotosResultDto
+
+interface PhotoRepository {
+    suspend fun searchPhotos(query: String): SearchPhotosResultDto
+}
